954188b6cec55c82618f1d43d47d4d42ff21b438
Andrew Alsing <andrew32alsing@gmail.com>
2023-04-20 10:28:22 -0700

    updated colors to pass contrast accessibility test

--- src/components/RandomSpace/RandomSpace.js
+++ src/components/RandomSpace/RandomSpace.js
@@ -1,20 +1,22 @@
-import { useNavigate } from 'react-router-dom'
-import './RandomSpace.css';
-import data from '../../sfpopos-data.json'
+import { useNavigate } from "react-router-dom";
+import "./RandomSpace.css";
+import data from "../../sfpopos-data.json";
 
 function RandomSpace() {
-	const navigate = useNavigate()
+  const navigate = useNavigate();
 
   return (
-	<button 
-        className="RandomSpace"
-        aria-label="Show me a random space button"
-        onClick={(e) => {
-                const id = Math.floor(Math.random() * data.length)
-                navigate(`/details/${id}`)
-            }}>Show me a random space
+    <button
+      className="RandomSpace"
+      aria-label="Show me a random space button"
+      onClick={(e) => {
+        const id = Math.floor(Math.random() * data.length);
+        navigate(`/details/${id}`);
+      }}
+    >
+      Show me a random space
     </button>
-  )
+  );
 }
 
-export default RandomSpace
+export default RandomSpace;
